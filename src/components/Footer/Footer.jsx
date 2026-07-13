40f0cf1967740abf75a29103119faa1f314ea04d
Erik <eazybeven@gmail.com>
2026-07-12 22:00:23 -0400

    my first update

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <p className="footer__text">Developed by Erik Mitchell</p>
+        <p className="footer__year">2026</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import "./Header.css";
+import logo from "../../images/logo.png";
+import avatar from "../../images/avatar.png";
+
+function Header({ handleAddClick }) {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} />
+      <p className="header__date-and-location">Date, Location</p>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        + Add Clothes
+      </button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
